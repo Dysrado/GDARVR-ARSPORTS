248ca9679a6494960acdb4091489eb3f42b493e8
Wayne Michael Pangilinan <113033667+Makiiidon@users.noreply.github.com>
2023-04-16 02:25:40 +0800

    Content locking

--- Assets/Script/Archery/ArcheryGroundPlane.cs
+++ Assets/Script/Archery/ArcheryGroundPlane.cs
@@ -8,6 +8,9 @@ public class ArcheryGroundPlane : MonoBehaviour
     [SerializeField] bool isContentPlaced = false;
     [SerializeField] Transform CameraPosition;
     [SerializeField] GameObject Content;
+    [SerializeField] GameObject btn;
+    bool grab = false;
+    bool canMove = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,24 @@ public class ArcheryGroundPlane : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (canMove)
+        {
+            if (isContentPlaced)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    RaycastHit hit;
+                    Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(r, out hit))
+                    {
+                        if (hit.collider.gameObject.CompareTag("Target"))
+                        {
+                            grab = !grab;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public void PositionContentAtPlaneAnchor(HitTestResult hit)
@@ -29,5 +49,24 @@ public class ArcheryGroundPlane : MonoBehaviour
             isContentPlaced = true;
         }
 
+    }
+
+    public void AutomaticHitResult(HitTestResult hit)
+    {
+        if (canMove)
+        {
+            if (grab)
+            {
+                Content.transform.position = hit.Position;
+                Content.transform.LookAt(new Vector3(-CameraPosition.position.x, 0, -CameraPosition.position.z));
+            }
+        }
+        
+    }
+
+    public void LockTarget()
+    {
+        canMove = false;
+        btn.SetActive(false);
     }
 }
